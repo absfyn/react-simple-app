4c537c5f8f9d834ebfb9fdeece26ec3dbb6f1e43
Sufyan Shaikh <absufyn@gmail.com>
2020-04-03 10:49:40 +0530

    files added.

---
+++ precache-manifest.efe51b7e82ae2a55976f3f0e5ed08268.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "75877967a1a60bc35e283abcd55e3468",
+    "url": "/react-simple-app/index.html"
+  },
+  {
+    "revision": "2bf4a436a6cec54cf034",
+    "url": "/react-simple-app/static/css/2.da8689d8.chunk.css"
+  },
+  {
+    "revision": "179bf3f80342613d12e2",
+    "url": "/react-simple-app/static/css/main.9a6145d1.chunk.css"
+  },
+  {
+    "revision": "2bf4a436a6cec54cf034",
+    "url": "/react-simple-app/static/js/2.d24925aa.chunk.js"
+  },
+  {
+    "revision": "870962a24b89ad1c4cfec6cddc04f648",
+    "url": "/react-simple-app/static/js/2.d24925aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0a0f7f7f0fd77200582e",
+    "url": "/react-simple-app/static/js/3.40f624ee.chunk.js"
+  },
+  {
+    "revision": "179bf3f80342613d12e2",
+    "url": "/react-simple-app/static/js/main.d2c46d33.chunk.js"
+  },
+  {
+    "revision": "807210c74cd836ac4a04",
+    "url": "/react-simple-app/static/js/runtime-main.170aa4c9.js"
+  }
+]);
